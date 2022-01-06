1d09a11b678fa7b697f2930ad91ecaba1db1d91d
ClaraShade <grzebyk.monika@gmail.com>
2022-01-06 20:52:56 +0100

    class GameScore defined to store the score of the Board best_move tuple printed based on the score

--- Enemy.py
+++ Enemy.py
@@ -17,28 +17,11 @@ class Square:
         self.is_empty = False
         self.char = 'o'
 
-#class GameScore:
-#    def __init__(self)
-
-class Board:
-    def __init__(self, side):
-        self.length = side
+class GameScore:
+    def __init__(self, length):
+        self.length = length
+        self.score = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self.squares = [['_', '_', 'o'], ['_', 'o', '_'], ['_', '_', '_']]
-#        self.score = GameScore()
-        self.score = [[0, 0, 0], [0, 0, 0] , [0, 0, 0]]
-
-    def printme(self):
-        rowcount = 0
-        for i in self.squares:
-            rowcount = rowcount + 1
-            if rowcount < 10:
-                print("Row "+str(rowcount)+" "+str(i))
-            else:
-                print("Row" + str(rowcount) + " " + str(i))
-        columns = "Column:"
-        for i in range(1, self.length+1):
-            columns = columns+str(i)+", "
-        print(columns)
 
     def eval_rows(self, player):
         for i in range(self.length):
@@ -138,13 +121,40 @@ class Board:
                 else:
                     self.score[i][i * -1 - 1] = self.score[i][i * -1 - 1] - 5
 
+    def evaluate_move(self, player):
+        self.eval_rows(player)
+        self.eval_cols(player)
+        self.eval_diag_l(player)
+        self.eval_diag_r(player)
+        print(self.score)
 
-def evaluate_move(board, player):
-    board.eval_rows(player)
-    board.eval_cols(player)
-    board.eval_diag_l(player)
-    board.eval_diag_r(player)
-    print(board.score)
+
+class Board:
+    def __init__(self, side):
+        self.length = side
+        self.squares = [['_', '_', 'o'], ['_', 'o', '_'], ['_', '_', '_']]
+        game_score = GameScore(side)
+        self.score = game_score
+
+
+    def evaluate_move(self, player):
+        return self.score.evaluate_move(player)
+
+    def printme(self):
+        rowcount = 0
+        for i in self.squares:
+            rowcount = rowcount + 1
+            if rowcount < 10:
+                print("Row "+str(rowcount)+" "+str(i))
+            else:
+                print("Row" + str(rowcount) + " " + str(i))
+        columns = "Column:"
+        for i in range(1, self.length+1):
+            columns = columns+str(i)+", "
+        print(columns)
+
+
+
 
 def show_move(board):
     moves = {}
@@ -152,10 +162,12 @@ def show_move(board):
         for j in range(board.length):
             movetuple = (i + 1, j + 1)
             print(movetuple)
-            moves[(i + 1, j + 1)] = board.score[i][j]
+            moves[(i + 1, j + 1)] = board.score.score[i][j]
 
     print(moves)
-    # print(max(list))
+    best_move = max(moves, key=moves.get)
+    print(best_move)
+
 
 
 new_board = Board(3)
@@ -163,7 +175,9 @@ print("Congratulations! You created " + str(3) + "-sided board, which has " + st
 print("See your board below:")
 new_board.printme()
 
+new_board.evaluate_move('x')
 
-evaluate_move(new_board, 'x')
+print(new_board.score.score)
 
 show_move(new_board)
+
