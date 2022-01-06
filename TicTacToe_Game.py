381725c394df0a368491118ddf1216fa7ba44171
ClaraShade <grzebyk.monika@gmail.com>
2022-01-06 21:35:10 +0100

    TicTacToe and Enemy combined

--- TicTacToe_Game.py
+++ TicTacToe_Game.py
@@ -31,6 +31,117 @@ class Square:
         self.char = 'o'
 
 
+class GameScore:
+    def __init__(self, length):
+        self.length = length
+        self.score = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.squares = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+
+    def eval_rows(self, player):
+        for i in range(self.length):
+            count_enemies = 0
+            count_friends = 0
+            for j in range(self.length):
+                mark = str(self.squares[i][j])
+                if mark == player:
+                    count_friends = count_friends + 1
+                elif mark == '_':
+                    pass
+                else:
+                    count_enemies = count_enemies + 1
+            for j in range(self.length):
+                mark = str(self.squares[i][j])
+                if mark == '_':
+                    if count_enemies == 0:
+                        self.score[i][j] = self.score[i][j] + 5
+                        if count_friends == self.length - 1:
+                            self.score[i][j] = self.score[i][j] + 10000
+                    elif count_enemies == self.length - 1:
+                        self.score[i][j] = self.score[i][j] + 500
+                    else:
+                        self.score[i][j] = self.score[i][j] - 5
+
+
+    def eval_cols(self, player):
+        for i in range(self.length):
+            count_enemies = 0
+            count_friends = 0
+            for j in range(self.length):
+                mark = str(self.squares[j][i])
+                if mark == player:
+                    count_friends = count_friends + 1
+                elif mark == '_':
+                    pass
+                else:
+                    count_enemies = count_enemies + 1
+            for j in range(self.length):
+                mark = str(self.squares[j][i])
+                if mark == '_':
+                    if count_enemies == 0:
+                        if count_friends == self.length - 1:
+                            self.score[j][i] = self.score[j][i] + 10000
+                        else:
+                            self.score[j][i] = self.score[j][i] + 5
+                    elif count_enemies == self.length - 1:
+                        self.score[j][i] = self.score[j][i] + 500
+                    else:
+                        self.score[j][i] = self.score[j][i] - 5
+
+    def eval_diag_l(self, player):
+        count_enemies = 0
+        count_friends = 0
+        for i in range(self.length):
+            mark = str(self.squares[i][i])
+            if mark == player:
+                count_friends = count_friends + 1
+            elif mark == '_':
+                pass
+            else:
+                count_enemies = count_enemies + 1
+        for i in range(self.length):
+            mark = str(self.squares[i][i])
+            if mark == '_':
+                if count_enemies == 0:
+                    if count_friends == self.length - 1:
+                        self.score[i][i] = self.score[i][i] + 10000
+                    else:
+                        self.score[i][i] = self.score[i][i] + 5
+                elif count_enemies == self.length - 1:
+                    self.score[i][i] = self.score[i][i] + 500
+                else:
+                    self.score[i][i] = self.score[i][i] - 5
+
+    def eval_diag_r(self, player):
+        count_enemies = 0
+        count_friends = 0
+        for i in range(self.length):
+            mark = str(self.squares[i][i * -1 - 1])
+            if mark == player:
+                count_friends = count_friends + 1
+            elif mark == '_':
+                pass
+            else:
+                count_enemies = count_enemies + 1
+        for i in range(self.length):
+            mark = str(self.squares[i][i * -1 - 1])
+            if mark == '_':
+                if count_enemies == 0:
+                    if count_friends == self.length - 1:
+                        self.score[i][i * -1 - 1] = self.score[i][i * -1 - 1] + 10000
+                    else:
+                        self.score[i][i * -1 - 1] = self.score[i][i * -1 - 1] + 5
+                elif count_enemies == self.length - 1:
+                    self.score[i][i * -1 - 1] = self.score[i][i * -1 - 1] + 500
+                else:
+                    self.score[i][i * -1 - 1] = self.score[i][i * -1 - 1] - 5
+
+    def evaluate_move(self, player):
+        self.eval_rows(player)
+        self.eval_cols(player)
+        self.eval_diag_l(player)
+        self.eval_diag_r(player)
+        print(self.score)
+
 class Board:
     def __init__(self, side):
         self.length = side
@@ -38,6 +149,8 @@ class Board:
         self.filled = 0
         self.full = False
         self.state = Result.not_finished
+        game_score = GameScore(side)
+        self.score = game_score
         square_index = 1
         for column in range(1, self.length + 1):
             row_list = []
@@ -47,6 +160,10 @@ class Board:
                     square_index = square_index + 1
                 row_list.append(new_square)
             self.squares.append(row_list)
+
+    def evaluate_move(self, player):
+        return self.score.evaluate_move(player)
+
 
     def printme(self):
         rowcount = 0
@@ -69,7 +186,121 @@ class Board:
         self.squares[mark_row][mark_column].set_o()
         self.filled = self.filled + 1
 
-''' 
+    def check_rows(self):
+        for i in range(self.length):
+            counter = 0
+            for j in range(self.length):
+                mark = str(self.squares[i][j])
+                if mark == 'x':
+                    counter = counter + 1
+                elif mark == 'o':
+                    counter = counter - 1
+                else:
+                    pass
+            if counter == int(self.length):
+                self.state = Result.win_x
+                return self.state
+            elif counter == int(self.length) * -1:
+                self.state = Result.win_o
+                return self.state
+            else:
+                if self.full:
+                    self.state = Result.draw
+                    return self.state
+                else:
+                    self.state = Result.not_finished
+
+    def check_cols(self):
+        for i in range(self.length):
+            counter = 0
+            for j in range(self.length):
+                mark = str(self.squares[j][i])
+                if mark == 'x':
+                    counter = counter + 1
+                elif mark == 'o':
+                    counter = counter - 1
+                else:
+                    pass
+            if counter == int(self.length):
+                self.state = Result.win_x
+                return self.state
+            elif counter == int(self.length) * -1:
+                self.state = Result.win_o
+                return self.state
+            else:
+                if self.full:
+                    self.state = Result.draw
+                    return self.state
+                else:
+                    self.state = Result.not_finished
+
+    def check_diagonal_l(self):
+        counter = 0
+        for i in range(self.length):
+            mark = str(self.squares[i][i])
+            if mark == 'x':
+                counter = counter + 1
+            elif mark == 'o':
+                counter = counter - 1
+            else:
+                pass
+        if counter == int(self.length):
+            self.state = Result.win_x
+            return self.state
+        elif counter == int(self.length) * -1:
+            self.state = Result.win_o
+            return self.state
+        else:
+            if self.full:
+                self.state = Result.draw
+                return self.state
+            else:
+                self.state = Result.not_finished
+
+    def check_diagonal_r(self):
+        counter = 0
+        for i in range(self.length):
+            mark = str(self.squares[i][i*-1-1])
+            if mark == 'x':
+                counter = counter + 1
+            elif mark == 'o':
+                counter = counter - 1
+            else:
+                pass
+        if counter == int(self.length):
+            self.state = Result.win_x
+            return self.state
+        elif counter == int(self.length) * -1:
+            self.state = Result.win_o
+            return self.state
+        else:
+            if self.full:
+                self.state = Result.draw
+                return self.state
+            else:
+                self.state = Result.not_finished
+
+    def win_check(self):
+        self.check_rows()
+        if self.state != Result.not_finished:
+            pass
+        else:
+            self.check_cols()
+            if self.state != Result.not_finished:
+                pass
+            else:
+                self.check_diagonal_l()
+                if self.state != Result.not_finished:
+                    pass
+                else:
+                    self.check_diagonal_r()
+                    if self.state == Result.not_finished:
+                        if self.filled == self.length ** 2:
+                            self.state = Result.draw
+                        else:
+                            pass
+
+
 def outcome(board):
     board.win_check()
     if board.state == Result.win_x:
@@ -83,12 +314,29 @@ def outcome(board):
         end_game()
     else:
         print("Next move!")
-'''
 
 def end_game():
     print("Thank you! Your game has ended!")
     exit()
 
+def change(player):
+    if player == 'x':
+        player = 'o'
+    else:
+        player = 'x'
+    return player
+
+def show_move(board):
+    moves = {}
+    for i in range(board.length):
+        for j in range(board.length):
+            movetuple = (i + 1, j + 1)
+            #print(movetuple)
+            moves[(i + 1, j + 1)] = board.score.score[i][j]
+
+    #print(moves)
+    #best_move = max(moves, key=moves.get)
+    #print(best_move)
 
 def play(anyboard):
     while anyboard.state == Result.not_finished:
@@ -108,11 +356,16 @@ def play(anyboard):
             print("Mark x in: ")
             mark_row = int(input("row: ")) - 1
             mark_column = int(input("column: ")) - 1
-
+#anyboard.mark_x to może też działać na Enemy
         anyboard.mark_x(mark_row, mark_column)
         print("Thank you! See your board below: ")
         anyboard.printme()
-        #outcome(anyboard)
+        new_board.evaluate_move('x')
+
+        print(new_board.score.score)
+
+        show_move(new_board)
+        outcome(anyboard)
 
         if anyboard.state == Result.not_finished:
             print("Mark o in: ")
@@ -128,7 +381,7 @@ def play(anyboard):
             anyboard.mark_o(mark_row, mark_column)
             print("Thank you! See your board below: ")
             anyboard.printme()
-            #outcome(anyboard)
+            outcome(anyboard)
             if anyboard.state != Result.not_finished:
                 end_game()
             else:
@@ -151,5 +404,5 @@ print("Congratulations! You created " + str(side) + "-sided board, which has " +
 print("See your board below:")
 new_board.printme()
 
-
+player = 'x'
 play(new_board)
